a3ee3a76c701d4b9abfe23e35b96f009bbfb13b5
MC-no-circle <595969887@qq.com>
2024-05-26 20:09:58 +0800

    添加个人简介

---
+++ js/memoris.js
@@ -0,0 +1,9 @@
+function special() {
+    var inputText = document.getElementById("special").value;
+
+    if (inputText === "包佳禾") {
+        alert("我真的喜欢你！");
+    } else if (inputText === "刘涵语") {
+        alert("加油，我在前方等你！");
+    }
+}
