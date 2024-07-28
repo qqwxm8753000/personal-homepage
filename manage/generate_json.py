e693abc31c4fff97858667a13cd7a1efc606da53
qqwxm8753000 <595969887@qq.com>
2024-07-28 20:52:18 +0800

    add auto get china trackers

--- manage/generate_json.py
+++ manage/generate_json.py
@@ -2,6 +2,10 @@ import os
 import json
 from typing import List, Dict
 from codecs import open
+import requests
+import zipfile
+
+
 search_file_path = "./files"  # 文件搜索路径
 file_path = "./manage/files.json"  # json路径
 friend_json = "./manage/friend.json"
@@ -15,13 +19,13 @@ friend_link: List[dict] = [
 ]
 
 
-def search_file():
+def search_file() -> None:
     global data
     for i in os.listdir(search_file_path):
         data.append({"name": i, "link": f"/files/{i}"})
 
 
-def search_link():
+def search_link() -> None:
     global data
     for each in friend_link:
         data.append({
@@ -30,7 +34,7 @@ def search_link():
         })
 
 
-def save_friend():
+def save_friend() ->None:
     with open(friend_json, "w", encoding="utf-8") as f:
         f.write(json.dumps(data))
 
@@ -39,6 +43,20 @@ def save_files():
     with open(file_path, "w", encoding="utf-8") as f:
         f.write(json.dumps(data))
 
+def download_files(links:List[str]) ->None:
+    for item in links:
+        r = requests.get(item)
+        with open(f"./files/{item.split("/")[-1]}","wb") as f:
+            f.write(r.content)
+
+def unzip_files(file :str,folder :str) -> None:
+    with zipfile.ZipFile(file,'r') as zip_ref:
+        zip_ref.extractall(folder)
+
+download_files([
+    "https://gitee.com/harvey520/top.yaozuopan/raw/master/web/iplist.txt.zip"
+])
+unzip_files("./files/iplist.txt.zip","./files")
 
 if __name__ == "__main__":
     search_file()
